96a721fd6b16b4c58291960b7be217ef58620633
Kesha Hall <k_renee10@yahoo.com>
2021-03-31 15:21:04 -0400

    added code to generate random numbers

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = "";
+  var passwordText = document.querySelector("#password");
+  var length = 8;
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  
+
+  for (var i = 0; i < length ; i++) {
+      password += charset.charAt(Math.floor(Math.random() * length));
+  
+  
+  passwordText.value = password;
+  }
+  return password;
+  
+}
+ console.log(password);
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
